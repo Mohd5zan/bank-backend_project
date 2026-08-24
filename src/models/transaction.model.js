458d124c6ed341071b35a2453652a8716984653b
Mohd5zan <mohd5zan77@gmail.com>
2026-08-24 14:14:01 +0530

    transaction Model created

---
+++ src/models/transaction.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const transactionSchema = new mongoose.Schema(
+  {
+    fromAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+      require: [true, "Transaction must be associated with a sender account"],
+      index: true,
+    },
+    toAccount: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+      require: [true, "Transaction must be associated with a receiver account"],
+      index: true,
+    },
+    status: {
+      type: String,
+      enum: {
+        value: ["PENDING", "COMPLETED", "FAILED", "REVERSED"],
+        message: "Status can either be PENDING,COMPLETED,FAILED or REVERSED",
+      },
+      default: "PENDING",
+    },
+    amount: {
+      type: Number,
+      required: [true, "Amount needed"],
+      min: [0, "Amount cannot be negative"],
+    },
+    idempotencyKey:{
+        type:String,
+        required:[true,"Idempotency Key is required for creating a transaction"],
+        unique:true,
+        index:true,
+    }
+  },
+  { timestamps: true },
+);
+
+const transactionModel=mongoose.model('Transaction',transactionSchema);
+module.exports=transactionModel;
